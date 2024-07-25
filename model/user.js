1cdd6d6c94a176238130f1b2fb011bb22d4d5051
ArgosNevrikos <140239328+ArgosNevrikos@users.noreply.github.com>
2024-07-25 14:31:13 +0800

    copy

---
+++ model/user.js
@@ -0,0 +1,20 @@
+// model/user.js
+const mongoose = require('mongoose');
+const AutoIncrement = require('mongoose-sequence')(mongoose);
+
+const userSchema = new mongoose.Schema({
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, required: true },
+    image: { type: String, default: null },
+    userID: { type: Number, unique: true }
+});
+
+// Add the auto-increment plugin to the schema
+userSchema.plugin(AutoIncrement, { inc_field: 'userID'});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
